b292cd2f277e828bf1ee46f24886f97631ae93e1
Daniel Prosi <69299070+ProsiDecon@users.noreply.github.com>
2025-05-23 12:25:35 +0200

    Change algorihm to exploit column-major ordering and make explicit row-wise and column-wise

--- neighborhood_smoothing.jl
+++ neighborhood_smoothing.jl
@@ -11,8 +11,9 @@ using SparseArrays
 #     First for-loop was changed to allow for asymmetric networks
 #     """
 
-
-function NeighborhoodSmoothing(A::SparseMatrixCSC; directed::Bool = false)
+#=
+# old row-wise version, inefficient
+function NeighborhoodSmoothing_rowwise(A::SparseMatrixCSC; directed::Bool = false)
 
     # INPUT:
     # A: adjacency matrix (NxN)
@@ -58,6 +59,67 @@ function NeighborhoodSmoothing(A::SparseMatrixCSC; directed::Bool = false)
     end
 
     return W_hat
+end
+=#
+
+
+### column-major ordered version (which for large matrices is substantially faster in Julia)
+function NeighborhoodSmoothing(A::Union{SparseMatrixCSC, Matrix}; directed::Bool = false, direction::Symbol = :columnwise)
+
+    # INPUT:
+    # A: adjacency matrix (NxN)
+    # OUTPUT:
+    # W_hat: estimated probability matrix
+    # Note for directed graphs that the transposition-order of the input matrix matters. 
+    # "forward steps" on the matrix (in the sense of finding the neighbors via one forward and one backward step)
+    # are steps along the row of the matrix (row-elements are outflows from node)
+
+    if direction == :rowwise    # for row-wise comparisons, run operations on the transpose and then transpose output at the end
+        A = copy(transpose(A))
+    elseif direction != :columnwise
+        throw("direction must be :rowwise or :columnwise")
+    end
+
+    N = size(A, 1)
+    h = sqrt(log(N) / N)
+
+    # Compute dissimilarity measures
+    D = zeros(N, N)     # sparse matrix writing is not threadsafe
+
+    if directed
+        A_sq = A' * A / N
+    else
+        A_sq = A * A / N
+    end
+
+    Threads.@threads for idx in 1:(N * N)   # this loop is effectively a double-loop but using efficient multi-threading
+        i = (idx - 1) ÷ N + 1
+        j = (idx - 1) % N + 1
+        if i != j
+            D[i, j] = maximum(abs.(A_sq[:, i] .- A_sq[:, j]))
+        end
+    end
+
+    Kernel_mat = zeros(Float64, N, N)
+    for i in 1:N
+        threshold = quantile(D[:, i], h)
+        Kernel_mat[:, i] .= D[:, i] .< threshold
+    end
+
+    # Normalize each row under L1 norm
+    col_sums = sum(Kernel_mat, dims=1) .+ 1e-10
+    Kernel_mat = Kernel_mat ./ col_sums
+
+    W_hat = A * Kernel_mat
+    if !directed
+        W_hat = (W_hat + W_hat') / 2
+    end
+
+    if direction == :columnwise
+        return W_hat
+    else
+        return Matrix(W_hat')
+    end
 end
 
 
