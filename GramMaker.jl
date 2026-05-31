a8b0919989021fc941cc911a3425ca2da49e2f8c
Daniel Prosi <69299070+ProsiDecon@users.noreply.github.com>
2026-05-31 17:23:32 +0200

    new pipeline with A and gram in struct

--- GramMaker.jl
+++ GramMaker.jl
@@ -1,5 +1,6 @@
-struct PSDKernel
-    gram::AbstractMatrix
+mutable struct PSDKernel
+    gram::Matrix
+    A::AbstractMatrix
     type::Symbol
     k::Union{Int, Nothing}
     directed::Bool
@@ -14,33 +15,41 @@ function psdkernel(A::AbstractMatrix, type::Symbol, k::Union{Int, Nothing}, dire
     N = size(A, 1)
     @assert size(A, 1) == size(A, 2) "A must be a square matrix"
 
-    T = eltype(A)
-    
-    gram = Matrix{T}(I, N, N)   # allocate the gram matrix as identity so we can later in-place rmul! it with the kernel matrix
+    gram = Matrix{Float64}(I, N, N)   # allocate the gram matrix as identity 
 
-    return PSDKernel(gram, type, k, directed, direction, false)
+    return PSDKernel(gram, A, type, k, directed, direction, false)
 end
 
 ### functions to generate spectral truncation of the kernel matrix
 """
+    Helper function to compute the low-rank approximation of the gram matrix based on the top k eigenvalues / singular values if  
+        the dense eigendecomposition / singular value decomposition is computed.
+"""
+function lowrank_gram!(M::AbstractMatrix, V::AbstractMatrix, weights::AbstractVector, normalizer::Real; add::Bool=false)
+    weighted_V = similar(M, size(V, 1), length(weights)) 
+    weighted_V .= V * Diagonal(weights ./ normalizer)
+    mul!(M, weighted_V, V', one(eltype(M)), add ? one(eltype(M)) : zero(eltype(M)))
+    return M
+end
+
+"""
     Computes the spectral truncation according to the first k eigenvalues and  
         returns the inner product (Gram matrix) based on this truncation.
 """
-function spectral_truncation!(M::Matrix, k::Int)
+function spectral_truncation!(M::Matrix, A::Matrix, k::Int)
     # compute the top k eigenvalues and eigenvectors of the symmetric matrix M
     N = size(M, 1)
     @assert k <= N "k must be less than or equal to the dimension of the matrix"
 
     if k == N
-        return M
+        error("For spectral truncation, specify truncation dimension k < N, the matrix dimension.")
     elseif k >= .1*N   # for large k, it is more efficient to compute the full eigendecomposition and then truncate
-        evals, evecs = eigen(M)
-        idx = sortperm(evals, rev=true)[1:k]
-        M .= evecs[:, idx] * Diagonal((evals[idx]).^2) * evecs[:, idx]'
-        M ./= N
+        evals, evecs = eigen(A)
+        idx = sortperm(evals; by = abs, rev=true)[1:k]
+        lowrank_gram!(M, view(evecs, :, idx), evals[idx].^2, N)
         return M
     else
-        evals, evecs = eigs(M, nev = k, which = :LM)
+        evals, evecs = eigs(A, nev = k, which = :LM)
         M .= evecs * Diagonal(evals.^2) * evecs'
         M ./= N
         return M
@@ -51,15 +60,39 @@ end
     Computes the spectral truncation according to the first k eigenvalues and  
         returns the inner product (Gram matrix) based on this truncation.
 """
-function spectral_truncation!(M::SparseMatrixCSC, k::Int)
+function spectral_truncation!(M::Matrix, A::Adjoint, k::Int)
     # compute the top k eigenvalues and eigenvectors of the symmetric matrix M
     N = size(M, 1)
     @assert k <= N "k must be less than or equal to the dimension of the matrix"
 
     if k == N
+        error("For spectral truncation, specify truncation dimension k < N, the matrix dimension.")
+    elseif k >= .1*N   # for large k, it is more efficient to compute the full eigendecomposition and then truncate
+        evals, evecs = eigen(A)
+        idx = sortperm(evals; by = abs, rev=true)[1:k]
+        lowrank_gram!(M, view(evecs, :, idx), evals[idx].^2, N)
         return M
     else
-        evals, evecs = eigs(M, nev = k, which = :LM)
+        evals, evecs = eigs(A, nev = k, which = :LM)
+        M .= evecs * Diagonal(evals.^2) * evecs'
+        M ./= N
+        return M
+    end
+end
+
+"""
+    Computes the spectral truncation according to the first k eigenvalues and  
+        returns the inner product (Gram matrix) based on this truncation.
+"""
+function spectral_truncation!(M::Matrix, A::SparseMatrixCSC, k::Int)
+    # compute the top k eigenvalues and eigenvectors of the symmetric matrix M
+    N = size(M, 1)
+    @assert k <= N "k must be less than or equal to the dimension of the matrix"
+
+    if k == N
+        error("For spectral truncation, specify truncation dimension k < N, the matrix dimension.")
+    else
+        evals, evecs = eigs(A, nev = k, which = :LM)
         M .= evecs * Diagonal(evals.^2) * evecs'
         M ./= N
         return M
@@ -70,32 +103,19 @@ end
     Computes the singular truncation according to the first k singular values and  
         returns the inner product (Gram matrix) based on this truncation in column-direction (i.e. A'A).
 """
-function singular_truncation!(M::Matrix, k::Int)
+function singular_truncation!(M::Matrix, A::Matrix, k::Int)
     N = size(M, 1)
     @assert k <= N "k must be less than or equal to the dimension of the matrix"
 
     if k == N
+        error("For singular truncation, specify truncation dimension k < N, the matrix dimension.")
+    elseif k >= .1*N 
+        U, S, _ = svd(A)
+        idx = sortperm(S; by = abs, rev=true)[1:k]
+        lowrank_gram!(M, view(U, :, idx), S[idx].^2, N)
         return M
     else
-        U, S, _ = svd(M)
-        M .= U[:, 1:k] * Diagonal(S[1:k].^2) * U[:, 1:k]'
-        M ./= N
-        return M
-    end
-end
-
-"""
-    Computes the singular truncation according to the first k singular values and  
-        returns the inner product (Gram matrix) based on this truncation in column-direction (i.e. A'A).
-"""
-function singular_truncation!(M::SparseMatrixCSC, k::Int)
-    N = size(M, 1)
-    @assert k <= N "k must be less than or equal to the dimension of the matrix"
-
-    if k == N
-        return M
-    else
-        U, S, _ = svds(M, nev = k, which = :LM)
+        U, S, _ = svds(A, nsv = k, which = :LM)
         M .= U * Diagonal(S.^2) * U'
         M ./= N
         return M
@@ -106,15 +126,87 @@ end
     Computes the singular truncation according to the first k singular values and  
         returns the inner product (Gram matrix) based on this truncation in column-direction (i.e. A'A).
 """
-function singular_truncation_both!(M::Matrix, k::Int)
+function singular_truncation!(M::Matrix, A::Adjoint, k::Int)
     N = size(M, 1)
     @assert k <= N "k must be less than or equal to the dimension of the matrix"
 
     if k == N
+        error("For singular truncation, specify truncation dimension k < N, the matrix dimension.")
+    elseif k >= .1*N 
+        U, S, _ = svd(A)
+        idx = sortperm(S; by = abs, rev=true)[1:k]
+        lowrank_gram!(M, view(U, :, idx), S[idx].^2, N)
         return M
     else
-        U, S, V = svd(M)
-        M .= U[:, 1:k] * Diagonal(S[1:k].^2) * U[:, 1:k]' + V[:, 1:k] * Diagonal(S[1:k].^2) * V[:, 1:k]'
+        U, S, _ = svds(A, nsv = k, which = :LM)
+        M .= U * Diagonal(S.^2) * U'
+        M ./= N
+        return M
+    end
+end
+
+"""
+    Computes the singular truncation according to the first k singular values and  
+        returns the inner product (Gram matrix) based on this truncation in column-direction (i.e. A'A).
+"""
+function singular_truncation!(M::Matrix, A::SparseMatrixCSC, k::Int)
+    N = size(M, 1)
+    @assert k <= N "k must be less than or equal to the dimension of the matrix"
+
+    if k == N
+        error("For singular truncation, specify truncation dimension k < N, the matrix dimension.")
+    else
+        U, S, _ = svds(A, nsv = k, which = :LM)
+        M .= U * Diagonal(S.^2) * U'
+        M ./= N
+        return M
+    end
+end
+
+"""
+    Computes the singular truncation according to the first k singular values and  
+        returns the inner product (Gram matrix) based on this truncation in column-direction (i.e. A'A).
+"""
+function singular_truncation_both!(M::Matrix, A::Matrix, k::Int)
+    N = size(M, 1)
+    @assert k <= N "k must be less than or equal to the dimension of the matrix"
+
+    if k == N
+        error("For singular truncation, specify truncation dimension k < N, the matrix dimension.")
+    elseif k >= .1*N 
+        U, S, V = svd(A)
+        idx = sortperm(S; by = abs, rev=true)[1:k]
+        lowrank_gram!(M, view(U, :, idx), S[idx].^2, 2*N)
+        lowrank_gram!(M, view(V, :, idx), S[idx].^2, 2*N; add=true) # add = true adds the object to existing M
+        return M
+    else
+        U, S, V = svds(A, nsv = k, which = :LM)
+        M .= U * Diagonal(S.^2) * U' + V * Diagonal(S.^2) * V'
+        M ./= 2*N
+        return M
+    end
+end
+
+
+"""
+    Computes the singular truncation according to the first k singular values and  
+        returns the inner product (Gram matrix) based on this truncation in column-direction (i.e. A'A).
+"""
+function singular_truncation_both!(M::Matrix, A::Adjoint, k::Int)
+    N = size(M, 1)
+    @assert k <= N "k must be less than or equal to the dimension of the matrix"
+
+    if k == N
+        error("For singular truncation, specify truncation dimension k < N, the matrix dimension.")
+    elseif k >= .1*N 
+        U, S, V = svd(A)
+        idx = sortperm(S; by = abs, rev=true)[1:k]
+        lowrank_gram!(M, view(U, :, idx), S[idx].^2, 2*N)
+        lowrank_gram!(M, view(V, :, idx), S[idx].^2, 2*N; add=true) # add = true adds the object to existing M
+        return M
+    else
+        U, S, V = svds(A, nsv = k, which = :LM)
+        M .= U * Diagonal(S.^2) * U' + V * Diagonal(S.^2) * V'
         M ./= 2*N
         return M
     end
@@ -124,14 +216,14 @@ end
     Computes the singular truncation according to the first k singular values and  
         returns the inner product (Gram matrix) based on this truncation in column-direction (i.e. A'A).
 """
-function singular_truncation_both!(M::SparseMatrixCSC, k::Int)
+function singular_truncation_both!(M::Matrix, A::SparseMatrixCSC, k::Int)
     N = size(M, 1)
     @assert k <= N "k must be less than or equal to the dimension of the matrix"
 
     if k == N
-        return M
+        error("For singular truncation, specify truncation dimension k < N, the matrix dimension.")
     else
-        U, S, V = svds(M, nev = k, which = :LM)
+        U, S, V = svds(A, nsv = k, which = :LM)
         M .= U * Diagonal(S.^2) * U' + V * Diagonal(S.^2) * V'
         M ./= 2*N
         return M
@@ -142,34 +234,35 @@ end
     Fits the kernel to the input matrix A and initializes the gram matrix.
 """
 function fit_initialised!(kernel::PSDKernel)
-    if kernel.k >= size(kernel.gram, 1)
+    N = size(kernel.A,1)
+
+    if !isnothing(kernel.k) && kernel.k >= N
         @warn "k is larger than the dimension of the graph. Returning full gram matrix."
         kernel.k = nothing
     end
-    if typeof(kernel.k) <:Int
+
+    if (typeof(kernel.k) <:Int) && (kernel.k < N)
         if !kernel.directed
-            spectral_truncation!(kernel.gram, kernel.k)
+            spectral_truncation!(kernel.gram, kernel.A, kernel.k)
         elseif kernel.direction == :columnwise
-            singular_truncation!(kernel.gram, kernel.k)
+            singular_truncation!(kernel.gram, kernel.A, kernel.k)
         elseif kernel.direction == :rowwise
-            singular_truncation!(kernel.gram', kernel.k)
+            singular_truncation!(kernel.gram, kernel.A', kernel.k)
         elseif kernel.direction == :both
-            singular_truncation_both!(kernel.gram, kernel.k)
+            singular_truncation_both!(kernel.gram, kernel.A, kernel.k)
         else
             error("direction must be either :columnwise, :rowwise, or :both")
         end
-    elseif isnothing(kernel.k)
+    elseif isnothing(kernel.k) || N == kernel.k
         if !kernel.directed
-            rmul!(kernel.gram, kernel.gram)   # if no truncation, the gram matrix is just the inner product of the choice of (symmetric) network matrix with itself
+            mul!(kernel.gram, kernel.A, kernel.A)   # if no truncation, the gram matrix is just the inner product of the choice of (symmetric) network matrix with itself
         elseif kernel.direction == :columnwise
-            lmul!(kernel.gram', kernel.gram)   # for columnwise, the gram matrix is A'A
+            mul!(kernel.gram, kernel.A', kernel.A)   # for columnwise, the gram matrix is A'A
         elseif kernel.direction == :rowwise
-            rmul!(kernel.gram, kernel.gram')   # for rowwise, the gram matrix is AA'
+            mul!(kernel.gram, kernel.A, kernel.A')   # for rowwise, the gram matrix is AA'
         elseif kernel.direction == :both
-            AAt = copy(kernel.gram * kernel.gram')
-            lmul!(kernel.gram', kernel.gram)   # for both, the gram matrix is (A'A + AA')/2
-            kernel.gram += AAt
-            kernel.gram ./= 2
+            mul!(kernel.gram, kernel.A', kernel.A)   # for both, this does A'A and allocates to kernel.gram
+            mul!(kernel.gram, kernel.A, kernel.A', 0.5, 0.5)  # this does 0.5 * (A'A) + 0.5 (AA')
         else
             error("direction must be either :columnwise, :rowwise, or :both")
         end
@@ -178,27 +271,26 @@ function fit_initialised!(kernel::PSDKernel)
     end
 end
 
-function fit!(kernel::PSDKernel, A::AbstractMatrix) 
+function fit!(kernel::PSDKernel) 
     if kernel.fitted
         @warn "Kernel already fitted. Re-fitting will overwrite the existing gram matrix."
-        kernel = psdkernel(A, kernel.type, kernel.k, kernel.directed, kernel.direction)   # re-initialize the gram matrix to identity before re-fitting
+        kernel .= psdkernel(kernel.A, kernel.type, kernel.k, kernel.directed, kernel.direction)   # re-initialize the gram matrix to identity before re-fitting
     end
 
     # this block stores in the kernel.gram the matrix of choice underlying the kernel
     if kernel.type == :adjacency                    # computes the Gram matrix on the neighborhood directly
-        rmul!(kernel.gram, A)
+        nothing
     elseif kernel.type == :neighborhood_smoothing   # computes the Gram matrix on Zhang, Levina, and Zhu's graphon estimate
-        P_hat = neighborhood_smoothing(A; directed = kernel.directed, direction = kernel.direction, returndist = false)
-        rmul!(kernel.gram, P_hat)
+        P_hat = neighborhood_smoothing(kernel.A; directed = kernel.directed, direction = kernel.direction, returndist = false)
+        kernel .= psdkernel(P_hat, kernel.type, kernel.k, kernel.directed, kernel.direction)
     elseif kernel.type == :laplacian                   # computes the Gram matrix on the unnormalized graph Laplacian
-        L = Diagonal(sum(A, dims=2))
-        L -= A
-        rmul!(kernel.gram, L)
+        indeg = vec(sum(A, dims=2))
+        kernel.A .*= -1
+        kernel.A += Diagonal(vec(sum(A, dims=2)))
     elseif kernel.type == :normalized_laplacian        # computes the Gram matrix on the normalized graph Laplacian
         @warn "The normalized Laplacian is in beta version."
-        D_inv_sqrt = Diagonal(1 ./ sqrt.(sum(A, dims=2)) .+ 1e-10)   # add small constant to avoid division by zero
-        L_norm = I - D_inv_sqrt * A * D_inv_sqrt
-        rmul!(kernel.gram, L_norm)
+        #D_inv_sqrt = Diagonal(1 ./ sqrt.(sum(kernel.A, dims=2)) .+ 1e-10)   # add small constant to avoid division by zero
+        #mul!(kernel.A)  .= I - D_inv_sqrt * A * D_inv_sqrt
     else
         error("type must be one of :adjacency, :neighborhood_smoothing, :laplacian, :normalized_laplacian")
     end
@@ -252,14 +344,19 @@ function gram_matrix(A::AbstractMatrix;                  # the adjacency matrix
     
     type in [:adjacency, :neighborhood_smoothing, :laplacian, :normalized_laplacian] || error("type must be one of :adjacency, :neighborhood_smoothing, :laplacian, :normalized_laplacian")
 
-    issymmetric = (A == A')
-    @assert !directed || !issymmetric "A is not symmetric, but directed is set to false."
-    if issymmetric
+    checksymmetric = issymmetric(A)
+    checkunweighted = all(map(x -> x ∈ [0;1], unique(A)))
+    @assert !directed || !checksymmetric "A is not symmetric, but directed is set to false."
+    if checksymmetric
         directed = false
     else
         if symmetrize
             A += A'
-            A ./= 2
+            if checkunweighted
+                A = (A .> 0)
+            else
+                A ./= 2
+            end 
             directed = false
         end
         (!directed || direction in [:columnwise, :rowwise, :both]) || error("direction must be either :columnwise, :rowwise, or :both")
