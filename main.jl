21e9e3a3389a8929df88b24ccf15f354965c3149
Daniel Prosi <69299070+ProsiDecon@users.noreply.github.com>
2026-06-02 16:40:40 +0200

    export more functions

--- main.jl
+++ main.jl
@@ -7,12 +7,16 @@ using Arpack
 using SparseArrays
 using Statistics
 
-export neighborhood_smoothing
-export gram_matrix
-
-
 
 include("neighborhood_smoothing.jl")        # Zhang, Levina and Zhu's Neighborhood Smoothing Algorithm
 include("GramMaker.jl")                     # function to compute the Gram Matrix of a graph for different kernels
+
+export neighborhood_smoothing
+
+export PSDKernel 
+export psdkernel
+export gram_matrix
+
+
 
 end
